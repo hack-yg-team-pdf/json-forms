fb6f135d62311bd5f34d02cd53ed356314798507
Thimothé Lamoureux <aparadox@gmail.com>
2018-11-16 18:13:57 -0800

    Non functional, for reference only

--- src/index.js
+++ src/index.js
@@ -6,11 +6,27 @@ import * as serviceWorker from './serviceWorker';
 import { combineReducers, createStore } from 'redux';
 import { Provider } from 'react-redux';
 import { Actions, jsonformsReducer } from '@jsonforms/core';
-import { person } from '@jsonforms/examples';
 import { materialFields, materialRenderers } from '@jsonforms/material-renderers';
+
+
+import { person } from '@jsonforms/examples';
+
+import db from './sample-db/all-forms';
+
 const schema = person.schema;
 const uischema = person.uischema
 const data = person.data;
+
+// TODO: This ain't the best place to deal with DB and fetching of the information.
+const test = db;
+db.map( (curr, id, val) => {
+    // curr is always a category
+    // Fetch all documents in the category and retrieve their JSON notation.
+    // Get the Schema and UI_Schema
+    
+});
+
+
 
 const store = createStore(
   combineReducers({ jsonforms: jsonformsReducer() }),  
@@ -25,7 +41,12 @@ const store = createStore(
 store.dispatch(Actions.init(data, schema, uischema));
 
 
-ReactDOM.render(<Provider store={store}><Form /></Provider>, document.getElementById('root'));
+ReactDOM.render(
+    <Provider store={store}>
+        <Form />
+    </Provider>
+    , document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
