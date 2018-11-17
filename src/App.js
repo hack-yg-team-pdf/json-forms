64b763f85edc8dcf9466c336226a1c80c14b7103
Thimothé Lamoureux <aparadox@gmail.com>
2018-11-17 02:12:45 -0800

    Remove unused resources

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import Sidebar from './components/Sidebar';
-import logo from './logo.svg';
 import './App.css';
 import { JsonForms } from '@jsonforms/react';
 
@@ -22,7 +21,7 @@ class App extends Component {
 
           <div className="json-forms">
           <h1>Title</h1>
-             <JsonForms />
+              <JsonForms />
           <button type="button" className="button">Submit Form</button> 
 
           </div>
