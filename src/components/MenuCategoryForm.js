cba66e25ad59046180fb32ac5937661e4763edad
Wesley George <wesley.george@gmail.com>
2018-12-17 13:42:21 -0500

    Inline MenuItemForm into MenuCategoryForm
    Why? I'm trying to make these links do something. With current app setup, I need to communicate to the top-level app that a particular link has been clicked; I am struggling to do this now, but will have an easier time once there are fewer layers.

--- src/components/MenuCategoryForm.js
+++ src/components/MenuCategoryForm.js
@@ -1,40 +1,23 @@
 import React, {Component} from 'react';
-import FormMenuItem from './MenuItemForm';
 
 class MenuCategoryForm extends Component {
-    constructor(props) {
-        super(props)
-
-        /*if (typeof props.clickHandle == "function" )
-            this.clickHandle = props.clickHandle/*.bind(this)*/
-
-    }
-
-    outputCategories() {
-
-    }
-
-
     render() {
-
         return (
             <div>
                 <h2>{this.props.name}</h2>
                 <ul className="json-list">
                     {
                         this.props.files.map( (file, i) => {
-                            let stop;
                             return(
-                                <FormMenuItem
-                                    app={this.props.app}x
-                                    id={file.id}
-                                    menuItemId={file.id}
-                                    name={file.label}
-                                    description={'Sample description'}
-                                    pdf={file.fname}
-                                    classes={'cool'}
-                                    clickHandle={this.props.clickHandle}
-                                />
+                                <li>
+                                    <a
+                                        id={`a-link-for-form-${file.id}`}
+                                        href="#"
+                                        onClick={this.props.clickHandle}
+                                    >
+                                        {file.label}
+                                    </a>
+                                </li>
                             );
                         })
                     }
